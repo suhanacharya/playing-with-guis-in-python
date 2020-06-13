e321549ce75bac9cd07afa843f8dbee6fda68276
suhanacharya <suhan.acharya1@gmail.com>
2020-06-13 22:52:18 +0530

    Initial Commit

---
+++ tkinter/hello.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.konichiwa = tk.Button(self)
+        self.konichiwa["text"] = "Konichiwa\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.konichiwa["command"] = self.say_konichiwa
+        self.hi_there.pack(side="top")
+        self.konichiwa.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+    
+    def say_konichiwa(self):
+        print("konichiwa, mina san!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
