53c6f6de61626cfe885ecb49513a82a289f0312c
suhanacharya <suhan.acharya1@gmail.com>
2020-06-15 23:23:24 +0530

    Completed simple calc

--- tkinter/micro projects/simlpe_gui_calc.py
+++ tkinter/micro projects/simlpe_gui_calc.py
@@ -6,24 +6,45 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+mem = []
+
+
+def button_click(number):
+    
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
+
+
+def button_clear():
+    e.delete(0, END)
+
 
 def button_add():
-    pass
+    mem.append(int(e.get()))
+    e.delete(0, END)
 
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+def button_equal():
+    mem.append(int(e.get()))
+    e.delete(0, END)
+    e.insert(0, str(sum(mem)))
+    mem.clear()
+    
+
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_add = Button(root, text="+", padx=40, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=86, pady=20, command=button_add)
-button_clear = Button(root, text="clear", padx=79, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=86, pady=20, command=button_equal)
+button_clear = Button(root, text="clear", padx=79, pady=20, command=button_clear)
 
 # Put button on screen
 
