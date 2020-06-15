dabb538fb0200dc243ca12a043ecfceed1eb9d24
suhanacharya <suhan.acharya1@gmail.com>
2020-06-15 23:52:22 +0530

    Added more operations

--- tkinter/micro projects/simlpe_gui_calc.py
+++ tkinter/micro projects/simlpe_gui_calc.py
@@ -6,8 +6,8 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-mem = []
-
+f_num = 0
+op = ""
 
 def button_click(number):
     
@@ -21,15 +21,51 @@ def button_clear():
 
 
 def button_add():
-    mem.append(int(e.get()))
+    global f_num
+    f_num = int(e.get())
+    global op 
+    op = "add"
+    e.delete(0, END)
+
+
+def button_subtract():
+    global f_num
+    f_num = int(e.get())
+    global op 
+    op = "sub"
+    e.delete(0, END)
+
+def button_multiply():
+    global f_num
+    f_num = int(e.get())
+    global op 
+    op = "mul"
+    e.delete(0, END)
+
+def button_divide():
+    global f_num
+    f_num = int(e.get())
+    global op 
+    op = "div"
     e.delete(0, END)
 
 
 def button_equal():
-    mem.append(int(e.get()))
+    s_num = int(e.get())
+    global op
+    global f_num
     e.delete(0, END)
-    e.insert(0, str(sum(mem)))
-    mem.clear()
+    if op == "add":
+        e.insert(0, f_num + s_num)
+    if op == "sub":
+        e.insert(0, f_num - s_num)
+    if op == "mul":
+        e.insert(0, f_num * s_num)
+    if op == "div":
+        e.insert(0, f_num // s_num)
+    # if op == "add":
+    #     e.insert(0, str(f_num) + str(s_num))
+
     
 
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -42,9 +78,14 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
 button_add = Button(root, text="+", padx=40, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=86, pady=20, command=button_equal)
-button_clear = Button(root, text="clear", padx=79, pady=20, command=button_clear)
+button_subtract = Button(root, text="-", padx=40, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
+
+button_equal = Button(root, text="  =  ", padx=80, pady=20, command=button_equal)
+button_clear = Button(root, text="clear", padx=80, pady=20, command=button_clear)
 
 # Put button on screen
 
@@ -62,7 +103,12 @@ button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
+
 button_add.grid(row=5, column=0)
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
 button_equal.grid(row=5, column=1, columnspan=2)
 
 # myButton = Button(root, text="Enter", command=myClick)
