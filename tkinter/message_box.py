a9005c3d33a57dfc814cecb8cb8192c2af8b6e31
suhanacharya <suhan.acharya1@gmail.com>
2020-06-19 23:52:36 +0530

    updated program

--- tkinter/message_box.py
+++ tkinter/message_box.py
@@ -8,7 +8,24 @@ root.iconbitmap('src/ninja_avatar.ico')
 
 
 def popup():
-	messagebox.showinfo("Help me Obi-wan Kenobi!", "You're my only hope")
+	response = messagebox.askokcancel("Help me Obi-wan Kenobi!", "You're my only " 
+														"hope")
+	#response = messagebox.showinfo("Help me Obi-wan Kenobi!", "You're my
+	# only " "hope")
+	# Label(root, text=response).pack()
+	# messagebox.showerror("Help me Obi-wan Kenobi!", "You're my only hope")
+	# messagebox.showerror("Help me Obi-wan Kenobi!", "You're my only hope")
+	# messagebox.askquestion("Help me Obi-wan Kenobi!", "You're my only hope")
+	# messagebox.askokcancel("Help me Obi-wan Kenobi!", "You're my only hope")
+	# messagebox.askyesno("Help me Obi-wan Kenobi!", "You're my only hope")
+	# messagebox.showwarning("Get out", "Go!!")
+
+	if response == 1:
+		Label(root, text="You clicked Yes!").pack()
+	else:
+		Label(root, text="You clicked NO!").pack()
+
+
 
 
 my_button = Button(root, text="Hope", command=popup)
